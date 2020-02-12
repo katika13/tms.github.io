2c57f8c280935ff6043581378f04078df462af6a
katika13 <benassy13@gmail.com>
2020-02-12 10:55:41 +0300

    hometask7 fix

--- JS-task7/assets/js/main.js
+++ JS-task7/assets/js/main.js
@@ -2,7 +2,7 @@ const calcBlock = document.getElementById("calc");
 const genBlock = document.getElementById("generator");
 const btnGen = document.querySelector("button");
 const resetBtn = document.getElementsByClassName("reset")[0];
-
+let calcElm = calcBlock.getElementsByTagName("input")[0];
 
 btnGen.addEventListener('click', function(e){ 
     let inputValue = genBlock.querySelector("input").value;
@@ -10,12 +10,10 @@ btnGen.addEventListener('click', function(e){
         alert('Введите число от 1 до 20');
     }else{
         genBlock.setAttribute("hidden", true);
-        console.log(inputValue);
         let calcBtnArr = []
         for (let i = 1; i <= inputValue; i++) {
             calcBtnArr.push(i);
         }
-        console.log(calcBtnArr);
         calcBlock.removeAttribute("hidden");
 
         const fragment = document.createDocumentFragment();
@@ -34,28 +32,30 @@ btnGen.addEventListener('click', function(e){
             div.appendChild(minusBtn);
             fragment.appendChild(div);
         }); 
-        calcBlock.insertBefore(fragment, resetBtn)
-
-        let calcElm = calcBlock.getElementsByTagName("input")[0];
-
-        calcBlock.addEventListener('click', e => {
-            if (e.target.className === 'plus-btn'){
-                calcElm.value = parseInt(calcElm.value) + parseInt(e.target.value);
-            }else if (e.target.className === 'minus-btn'){
-                calcElm.value = parseInt(calcElm.value) - parseInt(e.target.value);
-            }
-        });
-
-        resetBtn.addEventListener('click', function(e){
-            genBlock.removeAttribute("hidden");
-            calcBlock.setAttribute("hidden", true);
-            calcElm.value = 0;
-            calcBlock.removeChild(calcBlock);
-        });
+        let clearBlock = [...calcBlock.querySelectorAll('div')];
+        
+        clearBlock.forEach(clearBlock => {
+            clearBlock.remove();
+        })
+        calcElm.value = 0;
+        calcBlock.insertBefore(fragment, resetBtn);
     }
 });
 
+calcBlock.addEventListener('click', e => {
+    if (e.target.className === 'plus-btn'){
+        calcElm.value = parseInt(calcElm.value) + parseInt(e.target.value);
+    }else if (e.target.className === 'minus-btn'){
+        calcElm.value = parseInt(calcElm.value) - parseInt(e.target.value);
+    }
+});
 
+resetBtn.addEventListener('click', function(e){
+    genBlock.removeAttribute("hidden");
+    calcBlock.setAttribute("hidden", true);
+    calcElm.setAttribute('value', 0);
+    console.log(calcElm.value)
+});
 
 
   
